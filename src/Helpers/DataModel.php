522ee5b301d356c89a0a868fc34cbe0a0023833d
david_smith <david_smith@sweetwater.com>
2025-02-04 15:36:20 -0500

    feat/sdk Created `src/Helpers/DataModel.php`.

---
+++ src/Helpers/DataModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zerotoprod\SpapiLwaSdk\Helpers;
+
+use Zerotoprod\Transformable\Transformable;
+
+trait DataModel
+{
+    use \Zerotoprod\DataModel\DataModel;
+    use Transformable;
+}
