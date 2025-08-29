a9b5b541c9c02b6b0e81dc9c1f3ad4d1d055880b
GitHub Action <action@github.com>
2025-08-29 17:56:28 +0000

    Update docblocks with @link annotations

--- src/Helpers/DataModel.php
+++ src/Helpers/DataModel.php
@@ -4,6 +4,9 @@ namespace Zerotoprod\SpapiLwaSdk\Helpers;
 
 use Zerotoprod\Transformable\Transformable;
 
+/**
+ * @link https://github.com/zero-to-prod/spapi-lwa-sdk
+ */
 trait DataModel
 {
     use \Zerotoprod\DataModel\DataModel;
